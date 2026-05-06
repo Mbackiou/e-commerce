de1cc1b892a40ee75ad3232b4cf268bf5ffcba69
mbackiou <tourecheikhmbacke02@gmail.com>
2026-05-06 16:49:41 +0000

    Fix: Ajout du domaine Render aux ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)lj)%_9co#8f8#hnygtfkboj&k6z+f2c@i0utia9zi067p5j=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-backend-fc6d.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
