725baad254f7791d36a6214c667da998c4a4252f
mbackiou <tourecheikhmbacke02@gmail.com>
2026-05-17 13:47:56 +0000

    Add create superuser script

---
+++ backend/create_superuser.py
@@ -0,0 +1,15 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('pendalaye', 'penda@email.com', 'penda123')
+    print("Superuser créé !")
+else:
+    print("Superuser existe déjà.")
+    
